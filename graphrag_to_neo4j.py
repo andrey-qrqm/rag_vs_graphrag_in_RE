0805824224f02bc98ac62c3b7a2a97e804efdf50
Andrei Qub Madiarov <andrei.madiarov.ber@gmail.com>
2026-07-23 17:25:32 +0200

    pushed to neo4j

--- graphrag_to_neo4j.py
+++ graphrag_to_neo4j.py
@@ -5,7 +5,7 @@ import time
 import os
 
 load_dotenv()
-
+GRAPHRAG_FOLDER='./output'
 NEO4J_URI=os.getenv("NEO4J_URL")
 NEO4J_USERNAME=os.getenv("NEO4J_USERNAME")
 NEO4J_PASSWORD=os.getenv("NEO4J_PASSWORD")
@@ -45,7 +45,7 @@ for statement in statements:
         print(statement)
         driver.execute_query(statement)
 
-doc_df = pd.read_parquet(f'{GRAPHRAG_FOLDER}/create_final_documents.parquet', columns=["id", "title"])
+doc_df = pd.read_parquet(f'{GRAPHRAG_FOLDER}/documents.parquet', columns=["id", "title"])
 print(doc_df.head(2))
 
 statement = """
@@ -54,30 +54,27 @@ SET d += value {.title}
 """
 
 batched_import(statement, doc_df)
-
-text_df = pd.read_parquet(f'{GRAPHRAG_FOLDER}/create_final_text_units.parquet',
-                          columns=["id","text","n_tokens","document_ids"])
+text_df = pd.read_parquet(f'{GRAPHRAG_FOLDER}/text_units.parquet',
+                          columns=["id","text","n_tokens","document_id"])
 print(text_df.head(2))
 
 statement = """
 MERGE (c:__Chunk__ {id:value.id})
 SET c += value {.text, .n_tokens}
 WITH c, value
-UNWIND value.document_ids AS document
-MATCH (d:__Document__ {id:document})
+MATCH (d:__Document__ {id:value.document_id})
 MERGE (c)-[:PART_OF]->(d)
 """
 
 batched_import(statement, text_df)
-entity_df = pd.read_parquet(f'{GRAPHRAG_FOLDER}/create_final_entities.parquet',
-                            columns=["name","type","description","human_readable_id","id","description_embedding","text_unit_ids"])
+entity_df = pd.read_parquet(f'{GRAPHRAG_FOLDER}/entities.parquet',
+                            columns=["id","human_readable_id","title","type","description","text_unit_ids"])
 print(entity_df.head(2))
 
 entity_statement = """
 MERGE (e:__Entity__ {id:value.id})
-SET e += value {.human_readable_id, .description, name:replace(value.name,'"','')}
+SET e += value {.human_readable_id, .description, name:replace(value.title,'"','')}
 WITH e, value
-CALL db.create.setNodeVectorProperty(e, "description_embedding", value.description_embedding)
 CALL apoc.create.addLabels(e, case when coalesce(value.type,"") = "" then [] else [apoc.text.upperCamelCase(replace(value.type,'"',''))] end) yield node
 UNWIND value.text_unit_ids AS text_unit
 MATCH (c:__Chunk__ {id:text_unit})
@@ -86,33 +83,28 @@ MERGE (c)-[:HAS_ENTITY]->(e)
 
 batched_import(entity_statement, entity_df)
 
-rel_df = pd.read_parquet(f'{GRAPHRAG_FOLDER}/create_final_relationships.parquet',
-                         columns=["source","target","id","rank","weight","human_readable_id","description","text_unit_ids"])
+rel_df = pd.read_parquet(f'{GRAPHRAG_FOLDER}/relationships.parquet',
+                         columns=['id', 'human_readable_id', 'source', 'target', 'description', 'weight', 'combined_degree', 'text_unit_ids'])
 print(rel_df.head(2))
 
 rel_statement = """
     MATCH (source:__Entity__ {name:replace(value.source,'"','')})
     MATCH (target:__Entity__ {name:replace(value.target,'"','')})
-    // not necessary to merge on id as there is only one relationship per pair
     MERGE (source)-[rel:RELATED {id: value.id}]->(target)
-    SET rel += value {.rank, .weight, .human_readable_id, .description, .text_unit_ids}
+    SET rel += value {.combined_degree, .weight, .human_readable_id, .description, .text_unit_ids}
     RETURN count(*) as createdRels
 """
 
+
 batched_import(rel_statement, rel_df)
-community_df = pd.read_parquet(f'{GRAPHRAG_FOLDER}/create_final_communities.parquet', 
-                     columns=["id","level","title","text_unit_ids","relationship_ids"])
+community_df = pd.read_parquet(f'{GRAPHRAG_FOLDER}/communities.parquet', 
+                     columns=['id', 'human_readable_id', 'community', 'level', 'parent', 'children', 'title', 'entity_ids', 'relationship_ids', 'text_unit_ids', 'period', 'size'])
 
 print(community_df.head(2))
+
 statement = """
-MERGE (c:__Community__ {community:value.id})
-SET c += value {.level, .title}
-/*
-UNWIND value.text_unit_ids as text_unit_id
-MATCH (t:__Chunk__ {id:text_unit_id})
-MERGE (c)-[:HAS_CHUNK]->(t)
-WITH distinct c, value
-*/
+MERGE (c:__Community__ {community:value.community})
+SET c += value {.id, .human_readable_id, .level, .parent, .children, .title, .period, .size}
 WITH *
 UNWIND value.relationship_ids as rel_id
 MATCH (start:__Entity__)-[:RELATED {id:rel_id}]->(end:__Entity__)
@@ -123,15 +115,15 @@ RETURN count(distinct c) as createdCommunities
 
 batched_import(statement, community_df)
 
-community_report_df = pd.read_parquet(f'{GRAPHRAG_FOLDER}/create_final_community_reports.parquet',
-                               columns=["id","community","level","title","summary", "findings","rank","rank_explanation","full_content"])
+community_report_df = pd.read_parquet(f'{GRAPHRAG_FOLDER}/community_reports.parquet',
+                               columns=['id', 'human_readable_id', 'community', 'level', 'parent', 'children', 'title', 'summary', 'full_content', 'rank', 'rating_explanation', 'findings', 'full_content_json', 'period', 'size'])
 community_report_df.head(2)
 
 
 # import communities
 community_statement = """
 MERGE (c:__Community__ {community:value.community})
-SET c += value {.level, .title, .rank, .rank_explanation, .full_content, .summary}
+SET c += value {.level, .title, .rank, .rating_explanation, .full_content, .summary, .period, .size}
 WITH c, value
 UNWIND range(0, size(value.findings)-1) AS finding_idx
 WITH c, value, finding_idx, value.findings[finding_idx] as finding
